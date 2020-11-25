3b81479c4923b821e2a536a86982e2a3f68e9a12
francisco01 <franciscovn10@hotmail.com>
2020-11-25 09:39:08 -0300

    create serie

--- src/actions/serieFormActions.js
+++ src/actions/serieFormActions.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase';
+
 export const SET_FIELD = 'SET_FIELD';
 export const setField = (field, value) => {
     return {
@@ -7,6 +9,23 @@ export const setField = (field, value) => {
     }
 }
 
+export const SERIE_SAVED_SUCESS = 'SERIE_SAVED_SUCESS';
+const serieSavedSucess = () => ({
+    type: SERIE_SAVED_SUCESS
+})
+
 export const saveSerie = serie => {
-    
+    const {currentUser} = firebase.auth();
+    return async dispatch => {
+        try {
+            await firebase
+                .database()
+                .ref(`users/${currentUser.uid}/series`)
+                .push(serie);
+            dispatch(serieSavedSucess())
+        } catch (error) {
+            
+        }
+    }
+        
 }
--- src/pages/SerieFormPage.js
+++ src/pages/SerieFormPage.js
@@ -1,72 +1,116 @@
 import React from 'react';
-import { Text, View, StyleSheet, TextInput, Picker, Slider, Button, ScrollView, KeyboardAvoidingView } from 'react-native';
+import { 
+    Text, 
+    View, 
+    StyleSheet, 
+    TextInput, 
+    Picker, 
+    Slider, 
+    Button, 
+    ScrollView, 
+    KeyboardAvoidingView,
+    ActivityIndicator,
+    Alert,
+} from 'react-native';
 
 import { connect } from 'react-redux';
 import FormRow from '../components/FormRow';
 import { setField, saveSerie } from '../actions';
 
 
-const SerieFormPage = ({ serieForm, setField, saveSerie }) => (
-    <KeyboardAvoidingView behavior="padding" enabled keyboardVerticalOffset={100}>
-        <ScrollView>
-            <FormRow first>
-                <TextInput 
-                    style={styles.input}
-                    placeholder="Título"
-                    value={serieForm.title}
-                    onChangeText={value => setField('title', value )}
-                />
-            </FormRow>
-            <FormRow>
-                <TextInput 
-                    style={styles.input}
-                    placeholder="URL Image"
-                    value={serieForm.img}
-                    onChangeText={value => setField('img', value )}
-                />
-            </FormRow>
+class SerieFormPage extends React.Component{
 
-            <FormRow>
-                <Picker
-                    selectedValue={serieForm.gender}
-                    onValueChange={ itemValue => setField('gender',itemValue)} >
-                    <Picker.Item label="Ação" value="acao" />
-                    <Picker.Item label="Comédia" value="comedia" />
-                    <Picker.Item label="Romance" value="romance" />
-                    <Picker.Item label="Terror" value="terror" />
-                </Picker>
-        </FormRow>
-        <FormRow>
-            <View style={styles.sameRow}>
-                    <Text>Nota:</Text>
-                    <Text>{serieForm.rate}</Text>
-            </View>
-            <Slider 
-                onValueChange={value => setField('rate', value)}
-                value={serieForm.rate}
-                minimumValue={0}
-                maximumValue={100}
-                step={5}
-            />
-        </FormRow>
-        <FormRow first>
-                <TextInput 
-                    style={styles.input}
-                    placeholder="Descrição"
-                    value={serieForm.description}
-                    onChangeText={value => setField('description', value )}
-                    numberOfLines={5}
-                    multiline={true}
-                />
-            </FormRow>
-            <Button 
-                title="Salvar"
-                onPress={() => {
-                    saveSerie(serieForm)
-                }} />
-        </ScrollView>
-    </KeyboardAvoidingView>
-);
+    constructor(props){
+        super(props);
+
+        this.state = {
+            isLoading: false,
+        }
+    }
+    render(){
+        const {
+            serieForm, 
+            setField, 
+            saveSerie, 
+            navigation
+        } = this.props;
+
+        return (
+            <KeyboardAvoidingView behavior="padding" enabled keyboardVerticalOffset={100}>
+                <ScrollView>
+                    <FormRow first>
+                        <TextInput 
+                            style={styles.input}
+                            placeholder="Título"
+                            value={serieForm.title}
+                            onChangeText={value => setField('title', value )}
+                        />
+                    </FormRow>
+                    <FormRow>
+                        <TextInput 
+                            style={styles.input}
+                            placeholder="URL Image"
+                            value={serieForm.img}
+                            onChangeText={value => setField('img', value )}
+                        />
+                    </FormRow>
+        
+                    <FormRow>
+                        <Picker
+                            selectedValue={serieForm.gender}
+                            onValueChange={ itemValue => setField('gender',itemValue)} >
+                            <Picker.Item label="Ação" value="acao" />
+                            <Picker.Item label="Comédia" value="comedia" />
+                            <Picker.Item label="Romance" value="romance" />
+                            <Picker.Item label="Terror" value="terror" />
+                        </Picker>
+                </FormRow>
+                <FormRow>
+                    <View style={styles.sameRow}>
+                            <Text>Nota:</Text>
+                            <Text>{serieForm.rate}</Text>
+                    </View>
+                    <Slider 
+                        onValueChange={value => setField('rate', value)}
+                        value={serieForm.rate}
+                        minimumValue={0}
+                        maximumValue={100}
+                        step={5}
+                    />
+                </FormRow>
+                <FormRow first>
+                        <TextInput 
+                            style={styles.input}
+                            placeholder="Descrição"
+                            value={serieForm.description}
+                            onChangeText={value => setField('description', value )}
+                            numberOfLines={5}
+                            multiline={true}
+                        />
+                    </FormRow>
+                    {
+                        this.state.isLoading 
+                            ? <ActivityIndicator />
+                            : <Button 
+                                title="Salvar"
+                                onPress={ async () => {
+                                    this.setState({isLoading: true});
+                                    try{
+                                        await saveSerie(serieForm);
+                                        navigation.goBack();
+                                    }catch(error){
+                                        Alert.alert('Erro!', `Ocorreu eum erro - ${error.message}`);
+                                    }finally{
+                                        this.setState({isLoading: false});
+                                    }
+                                          
+                            }} />
+                    }
+                </ScrollView>
+            </KeyboardAvoidingView>
+        );
+    }
+}  
 
 const styles = StyleSheet.create({
     input: {
